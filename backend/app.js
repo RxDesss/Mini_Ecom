397e2e2a9ab3c7902afad3d5c10f6875a8e49ffc
RxDesss <rxdesss.com>
2024-06-27 17:08:44 +0530

    command env line

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const app=express();
-const dotenv=require('dotenv')
-const path=require('path')
-dotenv.config({path:path.join(__dirname,'config','config.env')})
+// const dotenv=require('dotenv')
+// const path=require('path')
+// dotenv.config({path:path.join(__dirname,'config','config.env')})
 
 const products=require('./routes/product');
 const orders=require('./routes/order');
@@ -12,6 +12,6 @@ app.use('/api/v1',orders);
 
 
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server listing to port ${process.env.PORT} in ${process.env.NODE_ENV}`)
+app.listen(3000,()=>{
+    console.log(`server listing to port 3000 in development dep}`)
 })
