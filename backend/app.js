027dc7f2c4cd136d5d8710e56d310e164fe9b577
RxDesss <rxdesss.com>
2024-06-28 11:25:56 +0530

    remove env file

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,6 @@
 const express=require('express');
 const app=express();
-// const dotenv=require('dotenv')
-// const path=require('path')
-// dotenv.config({path:path.join(__dirname,'config','config.env')})
+
 
 const products=require('./routes/product');
 const orders=require('./routes/order');
